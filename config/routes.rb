882ef8e6e227114676f4b01f454a0829dd39e9f2
Noviny BC <benjaminconolly@gmail.com>
2016-01-15 11:26:12 +1100

    partials fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+
   resources :searches
   resources :users
   resources :pages
+
+  root 'pages#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
