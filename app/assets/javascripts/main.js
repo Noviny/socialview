062b5ea19134423e853d8ff19e87763e9b91349d
Angie Ng <chueyng@gmail.com>
2016-01-16 17:28:15 +1100

    Instagram pictures is showing on google map

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,53 +1,112 @@
 $(document).ready(function () {
- // var search = function(e) {
- //   e.preventDefault(); //Prevent the default browser form submission.
+//  var search = function(e) {
+//   e.preventDefault(); //Prevent the default browser form submission.
 
-    // var query = $('#city_name').val();
-    //$.ajax('https://api.instagram.com/v1/media/popular?', {
-    //$.ajax('https://api.instagram.com/v1/locations/search?', {
-    $.ajax('https://api.instagram.com/v1/locations/{id=1&latitude=37.782&longitude=-122.387}?', {
-      type: "GET",
-      dataType: "jsonp",
-      data: {
-        // "id": "1",
-        // "name": "Dogpatch Labs",
-        // "latitude": 37.782,
-        // "longitude": -122.387,
-        access_token: '2583670140.1677ed0.386c99d44c5e4bf592e15f81625e8c79'
-      },
-     // }
-    }).done(function (info) {
-      // var date = [];
-      // var icon_image = [];
-      // var temp = [];
-      // var weather = [];
+  getLocation();
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition (
+          function (showPosition) {
+            var lat = showPosition.coords.latitude;
+            var lng = showPosition.coords.longitude;
+            console.log('Latitude: ', lat + 'Longitude: ', lng);
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 18,
+              center: {lat: lat, lng: lng}
+            });
+            dataRequest(map, lat, lng);
+          },
+          function(error) {
+            switch (geoPositionError.code) {
+                case 0: // UNKNOWN_ERROR
+                    alert('An unknown error occurred, sorry');
+                    break;
+                case 1: // PERMISSION_DENIED
+                    alert('Permission to use Geolocation was denied');
+                    break;
+                case 2: // POSITION_UNAVAILABLE
+                    alert("Couldn't find you...");
+                    break;
+                case 3: // TIMEOUT
+                    alert('The Geolocation request took too long and timed out');
+                    break;
+                default:
+            }
+          });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+  }
 
-      // $('#result').html('<h2>' + info['city']['name'] + ', ' + info['city']['country'] + '</h2>');
-      // $('#updated_date').html('<small>Last updated: ' +moment.unix(info['list'][0]['dt']).format('LLL')+ '</small>');
-
-      // for (var i = 0; i < info['list'].length; i++) {
-      //   date.push(moment.unix(info['list'][i]['dt']).format('dddd'));
-
-      //   var image = info['list'][i]['weather'][0]['icon'];
-      //   icon_image.push("http://openweathermap.org/img/w/" + image + '.png');
-      //   temp.push((info['list'][i]['temp']['day']).toFixed(1) + '°C');
-      //   weather.push(info['list'][i]['weather'][0]['description']);
-      // }
-      console.log(info);
-
-  //     $('#day').html('<ul><li><p><strong>' +date.join('</strong></p></li><li><p><strong>') + '</strong></p></li></ul>');
-  //     $('#temp').html('<ul><li>' +temp.join('</li><li>') + '</li></ul>');
-  //     icon_image.map(function(icon_url) {
-  //       var img = new Image();
-  //       img.src = icon_url;
-  //       img.id = 'icon';
-  //       $('#icons').append(img);
-  //     });
-  //     $('#status').html('<ul><li>' + weather.join('</li><li>') + '</li></ul>');
-  //   });
-  // };
-  // $('form').on('submit', search);
+  function dataRequest(map, lat, lng) {
+    $.ajax('http://api.instagram.com/v1/media/search/?', {
+        type: 'GET',
+        dataType: 'jsonp',
+        data: {
+          distance: 500,
+          count: 100,
+          lat: lat,
+          lng: lng,
+          access_token: '2583670140.1677ed0.386c99d44c5e4bf592e15f81625e8c79'
+        },
+        success: function(info) {
+          if (info.hasOwnProperty('data') && info.data.length > 0) {
+            var instPosts = [];
+            var i = 0;
+            _.each(info.data, function (itemRecord) {
+              instPosts[i] = [itemRecord.location.latitude, itemRecord.location.longitude, itemRecord.link, itemRecord.images.thumbnail.url];
+              i++
+            });
+            setInstPostMarkers(map, instPosts);
+          } //Else no photos show
+         }
     });
+  }
+
+  function setInstPostMarkers(map, instPosts) {
+    var markers = [];
+    var i = 0
+    _.each(instPosts, function (eachPost) {
+      var image = {
+        url: eachPost[3],
+        scaledSize: new google.maps.Size(64, 64),
+      };
+      var shape = {
+       coord: [12,4,216,22,212,74,157,70,184,111,125,67,6,56],
+       type: 'poly'
+      };
+      var pos = new google.maps.LatLng(
+          eachPost[0], eachPost[1]);
+      for (var j = 0; j < markers.length; j++) {
+        if (i != j && pos.equals(
+                markers[j].getPosition()
+            )) {
+            var newLat = pos.lat() *
+                (Math.random() *
+                    0.0002 + 1);
+            var newLng = pos.lng() *
+                (Math.random() *
+                    0.00003 + 1);
+            pos = new google.maps.LatLng(
+                newLat, newLng);
+        }
+      }
+      i++;
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        icon: image,
+        shape: shape,
+        zIndex: 2,
+        title: eachPost[2]
+      });
+
+      google.maps.event.addListener(
+          marker, 'click',
+          function() {
+            //showEmbed(this.title);
+      });
+      markers.push(marker);
+    });
+  }
 });
-
-
