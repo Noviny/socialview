d475e3f38918d8d785a90afd1c3a6e9526db0fb6
Harry Duncan <harryduncan@Harrys-MacBook-Air.local>
2016-01-18 18:15:29 +1100

    finalized layout / sidebars are GONE

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -6,16 +6,35 @@ $(document).ready(function(){
       $('.-top').addClass('-on');
       $('.bg').addClass('-on');
   });
+
+  $('#login').on('click', function(e) {
+    console.log("made it this far");
+    e.preventDefault();
+    /* Act on the event */
+    $('.-login').addClass('-on');
+    $('.bg').addClass('-on');
+  });
+
+  $('#signup').on('click', function(e) {
+    console.log("made it this far");
+    e.preventDefault();
+    /* Act on the event */
+    $('.-signup').addClass('-on');
+    $('.bg').addClass('-on');
+  });
+
   $('#left').on('click', function(e) {
       e.preventDefault();
       /* Act on the event */
       $('.-left').addClass('-on');
   });
+
   $('#right').on('click', function(e) {
       e.preventDefault();
       /* Act on the event */
       $('.-right').addClass('-on');
   });
+
   $('.close').on('click', function(e) {
 
     e.preventDefault();
