8c672cbb59afedd71ad518af96b9f88def1e6bcd
Harry Duncan <harryduncan@Harrys-MacBook-Air.local>
2016-01-18 12:27:57 +1100

    sidebar features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114231644) do
+ActiveRecord::Schema.define(version: 20160117214159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20160114231644) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
   end
 
 end
