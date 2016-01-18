0f34135d31c52a8648c273ee2c5178ff94f20d00
Harry Duncan <harryduncan@Harrys-MacBook-Air.local>
2016-01-18 13:25:33 +1100

    Styling update

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,6 @@ ActiveRecord::Schema.define(version: 20160117235900) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "name"
