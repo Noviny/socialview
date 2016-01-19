71325829647986e9027fe65156b328262d78b1dd
Noviny BC <benjaminconolly@gmail.com>
2016-01-19 11:03:36 +1100

    working on advanced search wiring

---
+++ app/assets/javascripts/advancedSearch.js
@@ -0,0 +1,14 @@
+$(document).ready( function () {
+  var $searchType = $('#search-type')[0]
+  var $searchLocation = $('#search-location')[0]
+
+
+  $($searchType).on('click' function () {
+
+  })
+
+  $($searchLocation)
+
+  console.log('all info in search type ',$searchType)
+  console.log('all info in search location ',$searchLocation)
+})
